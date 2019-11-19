54174d93f63e40f23aec88535ce011aa402e7ff5
ljs <1103192570@qq.com>
2019-11-19 20:14:10 +0800

    xml解析csv验证过程完成

--- dsctest-job-web/dsctest-job-web-data-preprocessing/src/test/java/com/ljs/dsctest/dsctestjobwebdatapreprocessing/DsctestJobWebDataPreprocessingApplicationTests.java
+++ dsctest-job-web/dsctest-job-web-data-preprocessing/src/test/java/com/ljs/dsctest/dsctestjobwebdatapreprocessing/DsctestJobWebDataPreprocessingApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ljs.dsctest.dsctestjobwebdatapreprocessing;
 
+import com.ljs.dsctest.dsctestjobwebdatapreprocessing.XMLToCSV.conf.xmlcsvConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 
-@SpringBootTest
-class DsctestJobWebDataPreprocessingApplicationTests {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class DsctestJobWebDataPreprocessingApplicationTests {
+
+	//测试过程：
+//	@Autowired
+	private xmlcsvConfig document;
 
 	@Test
-	void contextLoads() {
+	public void readXMLJiegou() throws Exception{
+		ArrayList<String> destinct =document.ReadCSV();
+		Iterator iter = destinct.iterator();
+		while(iter.hasNext()){
+			String tmp= (String) iter.next();
+			System.out.println(tmp);
+		}
+		System.out.println("1");
 	}
 
 }
